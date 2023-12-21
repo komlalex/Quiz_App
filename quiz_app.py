6202fe4941c20891180101211a34798a3509e0ee
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-21 13:40:45 +0000

    Initial commit

---
+++ quiz_app.py
@@ -0,0 +1,8 @@
+"""
+# A dictionary that stores questions and answers
+# have a variable that tracks the score of the player
+# loop through the dictionary using the key vallue pairs
+# display each question to the user and allow them to answer
+# tell the if they are right or wrong
+# show the final result when the quiz is completed
+"""
